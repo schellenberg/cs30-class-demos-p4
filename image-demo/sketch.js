c298d3ab35ad7d701da4aa2ed5fc14dae39d7aab
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-09-23 13:54:24 -0600

    image demo

---
+++ image-demo/sketch.js
@@ -0,0 +1,17 @@
+// Image Demo
+// Sept 23, 2024
+
+let spongebob;
+
+function preload() {
+  spongebob = loadImage("bob.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  image(spongebob, mouseX, mouseY, spongebob.width * 0.5, spongebob.height * 0.5);
+}
