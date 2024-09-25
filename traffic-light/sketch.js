077ac8a87c0d0925d84ef2c6bbd48d1ce2741210
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-09-25 13:37:56 -0600

    traffic light demo

--- traffic-light/sketch.js
+++ traffic-light/sketch.js
@@ -1,10 +1,15 @@
-// Traffic Light Starter Code
-// Your Name Here
-// The Date Here
+// Traffic Light Demo
+// Dan Schellenberg
 
 // GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/#/p5/millis
+
+let lightState = "green";
+let lastTimeSwitched = 0;
+const GREEN_LIGHT_DURATION = 3000;
+const YELLOW_LIGHT_DURATION = 1000;
+const RED_LIGHT_DURATION = 4000;
 
 function setup() {
   createCanvas(600, 600);
@@ -13,6 +18,38 @@ function setup() {
 function draw() {
   background(255);
   drawOutlineOfLights();
+  switchStateIfNeeded();
+  displayCorrectLight();
+}
+
+function switchStateIfNeeded() {
+  if (lightState === "green" && millis() > lastTimeSwitched + GREEN_LIGHT_DURATION) {
+    lightState = "yellow";
+    lastTimeSwitched = millis();
+  }
+  else if (lightState === "yellow" && millis() > lastTimeSwitched + YELLOW_LIGHT_DURATION) {
+    lightState = "red";
+    lastTimeSwitched = millis();
+  }
+  else if (lightState === "red" && millis() > lastTimeSwitched + RED_LIGHT_DURATION) {
+    lightState = "green";
+    lastTimeSwitched = millis();
+  }
+}
+
+function displayCorrectLight() {
+  if (lightState === "green") {
+    fill("green");
+    ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  }
+  else if (lightState === "yellow") {
+    fill("yellow");
+    ellipse(width/2, height/2, 50, 50); //middle
+  }
+  else if (lightState === "red") {
+    fill("red");
+    ellipse(width/2, height/2 - 65, 50, 50); //top
+  }
 }
 
 function drawOutlineOfLights() {
