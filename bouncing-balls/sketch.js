3db1173685beb761918c0f602e7076221498738f
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-10-03 14:04:07 -0600

    bouncing ball demo

---
+++ bouncing-balls/sketch.js
@@ -0,0 +1,54 @@
+// Bouncing Ball Demo
+// Oct 3, 2024
+
+let ballArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 10; i++) {
+    spawnBall(width/2, height/2);
+  }
+}
+
+function draw() {
+  background(220);
+
+  for (let someBall of ballArray) {
+    //move ball
+    someBall.x += someBall.dx;
+    someBall.y += someBall.dy;
+  
+    //bounce if needed
+    if (someBall.x > width - someBall.radius || someBall.x < 0 + someBall.radius) {
+      someBall.dx *= -1;
+    }
+  
+    if (someBall.y > height - someBall.radius || someBall.y < 0 + someBall.radius) {
+      someBall.dy *= -1;
+    }
+  
+    //display ball
+    noStroke();
+    fill(someBall.red, someBall.green, someBall.blue, someBall.alpha);
+    circle(someBall.x, someBall.y, someBall.radius*2);
+  }
+}
+
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function spawnBall(theX, theY) {
+  let theBall = {
+    x: theX,
+    y: theY,
+    radius: random(30, 70),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    red: random(0, 255),
+    green: random(0, 255),
+    blue: random(0, 255),
+    alpha: random(0, 255),
+  };
+  ballArray.push(theBall);
+}
