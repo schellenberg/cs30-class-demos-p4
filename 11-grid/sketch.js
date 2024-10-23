4eae9a3c27f09fbda27dee8d31fc8da2896f70cb
schellenberg <227656+schellenberg@users.noreply.github.com>
2024-10-23 13:56:08 -0600

    Update sketch.js

--- 11-grid/sketch.js
+++ 11-grid/sketch.js
@@ -11,6 +11,7 @@
 let grid;
 const GRID_SIZE = 10;
 let cellSize;
+let shouldToggleNeighbours = true;
 
 function setup() {
   if (windowWidth < windowHeight) {
@@ -42,11 +43,27 @@ function mousePressed() {
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
-  if (grid[y][x] === 1) {
-    grid[y][x] = 0;
+  //toggle self
+  toggleCell(x, y);
+
+  //toggle neighbours
+  if (shouldToggleNeighbours) {
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
   }
-  else {
-    grid[y][x] = 1;
+}
+
+function toggleCell(x, y) {
+  //make sure the cell you're toggling is in the grid
+  if (x >= 0 && y >= 0 && x < GRID_SIZE && y < GRID_SIZE) {
+    if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+    else {
+      grid[y][x] = 1;
+    }
   }
 }
 
@@ -56,6 +73,9 @@ function keyPressed() {
   }
   if (key === "e") {
     grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+  if (key === "n") {
+    shouldToggleNeighbours = !shouldToggleNeighbours;
   }
 }
 
